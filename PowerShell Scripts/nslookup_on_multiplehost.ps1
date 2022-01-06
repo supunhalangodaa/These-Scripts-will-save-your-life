be9a5577e58171f6a10d32e713664f6b2cbf59bc
Supun Halangoda <supunhalangoda6+1@gmail.com>
2022-01-06 11:19:56 +0530

    Update nslookup_on_multiplehost.ps1

--- PowerShell Scripts/nslookup_on_multiplehost.ps1
+++ PowerShell Scripts/nslookup_on_multiplehost.ps1
@@ -1,1 +1,2 @@
 Get-Content C:\Temp\Hostname.txt | foreach {nslookup $_} >> C:\TEMP\nslookup.txt
+\\ Replace C:\Temp\Hostname.txt with a File with Hosts need to lookup and Replace C:\TEMP\nslookup.txt to a path that needs to save the output 
